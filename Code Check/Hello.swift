9e68f70245db8056e5b8b085b8702537b3cff64f
peepo3663 <peepo157@gmail.com>
2017-11-17 18:04:12 +0700

    add swift file

---
+++ Code Check/Hello.swift
@@ -0,0 +1,13 @@
+//
+//  Hello.swift
+//  CodeCheck
+//
+//  Created by Wasupol Tungsakultong on 17/11/2560 BE.
+//  Copyright © 2560 Wasupol Tungsakultong. All rights reserved.
+//
+
+import UIKit
+
+class Hello: NSObject {
+
+}
